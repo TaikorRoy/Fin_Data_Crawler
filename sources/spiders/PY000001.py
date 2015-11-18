0725890760cde0cdaf60bed0b6591752b9ad7389
wuxiaoshen <wuxiaoshen@shu.edu.cn>
2015-11-18 16:39:41 +0800

    v0.2
    V0.2

--- sources/spiders/PY000001.py
+++ sources/spiders/PY000001.py
@@ -3,6 +3,11 @@ __author__ = 'roy'
 
 class PY000001:
     def __init__(self):
+        self.url = url
+        self.crawling_time = None
+        self.source_name = None
+        self.crawl_id = None
+        self.data_id = None
         pass
 
     def crawl(self, job):
